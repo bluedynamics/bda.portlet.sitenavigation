0445ec4ce7245f3c8da9d68a7a35e8956c7fbe13
Johannes Raggam <raggam-nl@adm.at>
2015-03-06 02:10:15 +0100

    logging

--- src/bda/portlet/sitenavigation/upgrades/__init__.py
+++ src/bda/portlet/sitenavigation/upgrades/__init__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from plone.browserlayer.utils import unregister_layer
+import logging
+
+logger = logging.getLogger('bda.portlet.sitenavigation upgrade')
 
 
 def remove_browserlayer(context):
     try:
         unregister_layer(name=u"bda.portlet.sitenavigation")
+        logger.info('removed bda.portlet.sitenavigation browser layer')
     except KeyError:
         # No browser layer with name collective.geolocationbehavior registered
+        logger.warn('FAILED REMOVING bda.portlet.sitenavigation browser layer')
         pass
